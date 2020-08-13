1470a701447111a92547bc5b0583977664e75b76
mslein <maggieslein77@gmail.com>
2020-08-13 12:39:41 -0400

    cleaning up matrix and vis code

--- data vis /preliminary_variability.R
+++ data vis /preliminary_variability.R
@@ -1,13 +1,16 @@
 ####Preliminary plots of extracted current studies 
 install.packages("lubdriate")
 install.packages("ggpubr")
+install.packages("editrules")
 library(tidyverse)
 library(lubridate)
 library(dplyr)
 library(ggpubr)
+library(editrules)
 
-var_raw<-read.csv("varMATRIX.csv")
+var_raw<-read_csv("varMATRIX.csv")
 glimpse(var_raw)
+
 
 ##histograms for:
 #organization level overall
@@ -71,7 +74,7 @@ ggplot(var_raw, aes(x=generation_time_standardized_days, y=duration_standardized
 
 ###filtering for majority studies generation time
 var_majority<-filter(var_raw, generation_time_standardized_days < 110, duration_standardized_days < 100)
-var_flux<-filter(var_raw, flux_period_days < 5)
+var_narrow<-filter(var_majority, flux_period_days < 5)
 
 #scatterplot of GT vs experiment duration coded by organization level
 ggplot(var_majority, aes(x=generation_time_standardized_days, y=duration_standardized_days, shape=organization_level, color=organization_level))+
@@ -85,11 +88,11 @@ ggplot(var_raw, aes(x=generation_time_standardized_days, y=flux_period_days, sha
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   labs(title="Generation time (days) vs Period of fluctuation (days) by organization level")
 #scatterplot of GT vs period of fluctuation (less than five day period)
-ggplot(var_raw, aes(x=generation_time_standardized_days, y=flux_period_days, color=organization_level))+
-  geom_jitter(alpha=0.3, size=4)+
+ggplot(var_narrow, aes(x=as.numeric(generation_time_standardized_days), y=as.numeric(flux_period_days), color=organization_level))+
+  geom_jitter(alpha=0.3, size=4, position = position_jitter(width = 0.5, height = 0.5))+
   theme_classic()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  labs(title="Generation time (days) vs Period of fluctuation by organization level")
+  labs(title="Generation time (days) less than 100 days vs Period of fluctuation (days) less than 5 days by organization level")
 
 
 ######correlation plots#####
@@ -104,33 +107,4 @@ ggscatter(var_majority, x = "generation_time_standardized_days", y = "duration_s
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "Generation time (days)", ylab = "Experiment Duration (days)")
 
-#filtering by organization level 
-#individual 
-var_individual<-filter(var_majority, organization_level == "individual", generation_time_standardized_days > 0)
-
-#population 
-var_pop<-filter(var_majority, organization_level == "population", generation_time_standardized_days > 0)
-
-#community
-var_comm<-filter(var_majority, organization_level == "community", generation_time_standardized_days > 0)
-
-
-#generation time vs duration at individual level 
-a<-ggscatter(var_individual, x = "generation_time_standardized_days", y = "duration_standardized_days", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "kendall",
-          xlab = "Generation time (days)", ylab = "Experiment Duration (days)")
-
-#generation time vs duration at population level 
-b<-ggscatter(var_pop, x = "generation_time_standardized_days", y = "duration_standardized_days", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "kendall",
-          xlab = "Generation time (days)", ylab = "Experiment Duration (days)")
-#generation time vs duration at community level 
-c<-ggscatter(var_comm, x = "generation_time_standardized_days", y = "duration_standardized_days", 
-          add = "reg.line", conf.int = TRUE, 
-          cor.coef = TRUE, cor.method = "kendall",
-          xlab = "Generation time (days)", ylab = "Experiment Duration (days)")
-
-  
   
