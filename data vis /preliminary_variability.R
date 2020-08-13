a120fb4c123ad6d105272de922944ab244cad9f6
mslein <maggieslein77@gmail.com>
2020-08-13 11:28:34 -0400

    reorging repo

--- data vis /preliminary_variability.R
+++ data vis /preliminary_variability.R
@@ -14,7 +14,8 @@ glimpse(var_raw)
 plot1<-ggplot(var_raw, aes(x=organization_level))+
   geom_bar()+
   theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(title="Organization level distribution")
 
 #organizarion level colored by study 
 plot2<-ggplot(var_raw, aes(x=organization_level, fill=as.factor(study_number)))+
@@ -23,12 +24,19 @@ plot2<-ggplot(var_raw, aes(x=organization_level, fill=as.factor(study_number)))+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
   labs(title="Study organization level distribution")
 
-#organization level colored by variation level 
+#organization level colored by variation color 
 plot3<-ggplot(var_raw, aes(x=organization_level, fill=variation_color))+
   geom_bar()+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  labs(title="Variation in different organization levels")
+  labs(title="Variation color in different organization levels")
+
+#organization level colored by wave type 
+plot8<-ggplot(var_raw, aes(x=organization_level, fill=wave_type))+
+  geom_bar()+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(title="Variation wave type in different organization levels")
 
 #organization level broken out by independent variable
 plot4<-ggplot(var_raw, aes(x=organization_level, fill=independent_variable))+
@@ -49,6 +57,12 @@ plot6<-ggplot(var_raw, aes(x=dependent_variable))+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+#dependent variables by genus
+plot7<-ggplot(var_raw, aes(x=dependent_variable, fill=as.factor(organization_level)))+
+  geom_bar()+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 ###################
 ggplot(var_raw, aes(x=generation_time_standardized_days, y=duration_standardized_days, color=study_number))+
   geom_point()+
@@ -57,12 +71,26 @@ ggplot(var_raw, aes(x=generation_time_standardized_days, y=duration_standardized
 
 ###filtering for majority studies generation time
 var_majority<-filter(var_raw, generation_time_standardized_days < 110, duration_standardized_days < 100)
+var_flux<-filter(var_raw, flux_period_days < 5)
 
 #scatterplot of GT vs experiment duration coded by organization level
 ggplot(var_majority, aes(x=generation_time_standardized_days, y=duration_standardized_days, shape=organization_level, color=organization_level))+
   geom_jitter()+
   theme_classic()+
   labs(title="Generation time (days) vs Experiment duration (days) by organization level")
+#scatterplot of GT vs period of fluctuation 
+ggplot(var_raw, aes(x=generation_time_standardized_days, y=flux_period_days, shape=organization_level, color=organization_level))+
+  geom_jitter(alpha=0.5, size=4)+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(title="Generation time (days) vs Period of fluctuation (days) by organization level")
+#scatterplot of GT vs period of fluctuation (less than five day period)
+ggplot(var_raw, aes(x=generation_time_standardized_days, y=flux_period_days, color=organization_level))+
+  geom_jitter(alpha=0.3, size=4)+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(title="Generation time (days) vs Period of fluctuation by organization level")
+
 
 ######correlation plots#####
 #generation time vs duration--kendall correlation
